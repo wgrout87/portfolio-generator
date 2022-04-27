c77acff71dcf2261604b54228d6301974bf263ca
Will G <wgrout87@gmail.com>
2022-04-26 18:42:00 -0600

    generate html file

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    
-    
-    console.log('================');
-    
-    
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const fs = require("fs");
 
-printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
