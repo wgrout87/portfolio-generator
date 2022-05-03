4068ee7380a7cb3ab972feae2a103aa458b0a0bd
Will G <wgrout87@gmail.com>
2022-05-03 14:03:04 -0600

    user prompts

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-const generatePage = require('./src/page-template.js');
+const inquirer = require("inquirer");
 
-const fs = require("fs");
+// const fs = require("fs");
 
-const profileDataArgs = process.argv.slice(2);
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// const pageHTML = generatePage(name, github);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
